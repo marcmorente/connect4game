7b9d7c5477f8f453064014e31a7b19ffe03c7ef7
Marc Morente <mmorente@inthinking.co.uk>
2023-10-28 17:52:54 +0200

    Use cli to write the board

--- src/views/BoardView.ts
+++ src/views/BoardView.ts
@@ -1,23 +1,27 @@
 import { type Board } from '../models/Board'
+import { StandardCli } from './StandardCli'
 
 export class BoardView {
+  private readonly cli: StandardCli
+
   constructor (private readonly board: Board) {
     this.board = board
+    this.cli = StandardCli.getInstance()
   }
 
   write (): void {
-    console.log('\n')
+    this.cli.print('\n')
     for (let row: number = 0; row < this.board.getRows(); row++) {
       let rowString: string = ''
       for (let col: number = 0; col < this.board.getCols(); col++) {
         rowString += this.board.getToken(row, col).getColor()
       }
-      console.log(rowString)
+      this.cli.print(rowString)
     }
     let columnNumbers: string = ''
     for (let col: number = 1; col <= this.board.getCols(); col++) {
       columnNumbers += `${col} `
     }
-    console.log(columnNumbers)
+    this.cli.print(columnNumbers)
   }
 }
