44ac12729d167ef189a1a55b045860b9a8a2f39a
Marc <marc.morente@gmail.com>
2023-11-19 21:16:45 +0100

    Delete ControllerVisitor interface

--- src/controllers/ControllerVisitor.ts
+++
@@ -1,9 +0,0 @@
-import { type PlayController } from './PlayController'
-import { type ResumeController } from './ResumeController'
-import { type StartController } from './StartController'
-
-export interface ControllerVisitor {
-  visitStartController: (startController: StartController) => void
-  visitPlayController: (playController: PlayController) => void
-  visitResumeController: (resumeController: ResumeController) => void
-}
