a9c62b290c0cd0ff231b501b5b4d1b9f3eca5150
Marc Morente <marc.morente@gmail.com>
2023-11-28 06:56:42 +0100

    Refactor test setup functions in Board.test.ts

--- tests/models/Board.test.ts
+++ tests/models/Board.test.ts
@@ -21,12 +21,12 @@ describe('Board', () => {
   })
 
   it('should check for a winner in a diagonal line', () => {
-    setupDiagonalWin()
+    setupDiagonalWin(board)
     expect(board.checkWinner()).toBe(Color.YELLOW)
   })
 
   it('should check for a winner in a reverse diagonal line', () => {
-    setupReverseDiagonalWin()
+    setupReverseDiagonalWin(board)
     expect(board.checkWinner()).toBe(Color.YELLOW)
   })
 
@@ -34,41 +34,6 @@ describe('Board', () => {
     fillBoardWithoutWinning(board)
     expect(board.isTie()).toBe(true)
   })
-
-  function fillBoardWithoutWinning (board: Board): void {
-    for (let i = 0; i < board.cols; i++) {
-      const tokenColor = i % 2 === 0 ? Color.RED : Color.YELLOW
-      for (let j = 0; j < board.rows; j++) {
-        board.putToken(i, new Token(tokenColor))
-      }
-    }
-  }
-
-  function setupDiagonalWin (): void {
-    board.putToken(0, new Token(Color.YELLOW))
-    board.putToken(1, new Token(Color.RED))
-    board.putToken(1, new Token(Color.YELLOW))
-    board.putToken(2, new Token(Color.RED))
-    board.putToken(2, new Token(Color.RED))
-    board.putToken(2, new Token(Color.YELLOW))
-    board.putToken(3, new Token(Color.RED))
-    board.putToken(3, new Token(Color.RED))
-    board.putToken(3, new Token(Color.RED))
-    board.putToken(3, new Token(Color.YELLOW))
-  }
-
-  function setupReverseDiagonalWin (): void {
-    board.putToken(0, new Token(Color.RED))
-    board.putToken(0, new Token(Color.RED))
-    board.putToken(0, new Token(Color.RED))
-    board.putToken(0, new Token(Color.YELLOW))
-    board.putToken(1, new Token(Color.RED))
-    board.putToken(1, new Token(Color.RED))
-    board.putToken(1, new Token(Color.YELLOW))
-    board.putToken(2, new Token(Color.RED))
-    board.putToken(2, new Token(Color.YELLOW))
-    board.putToken(3, new Token(Color.YELLOW))
-  }
 })
 
 function addTokensInVertical (board: Board): void {
@@ -81,4 +46,39 @@ function addTokensInHorizontal (board: Board): void {
   for (let row = 0; row < board.rows; row++) {
     board.putToken(row, new Token(Color.RED))
   }
+}
+
+function fillBoardWithoutWinning (board: Board): void {
+  for (let i = 0; i < board.cols; i++) {
+    const tokenColor = i % 2 === 0 ? Color.RED : Color.YELLOW
+    for (let j = 0; j < board.rows; j++) {
+      board.putToken(i, new Token(tokenColor))
+    }
+  }
+}
+
+function setupDiagonalWin (board: Board): void {
+  board.putToken(0, new Token(Color.YELLOW))
+  board.putToken(1, new Token(Color.RED))
+  board.putToken(1, new Token(Color.YELLOW))
+  board.putToken(2, new Token(Color.RED))
+  board.putToken(2, new Token(Color.RED))
+  board.putToken(2, new Token(Color.YELLOW))
+  board.putToken(3, new Token(Color.RED))
+  board.putToken(3, new Token(Color.RED))
+  board.putToken(3, new Token(Color.RED))
+  board.putToken(3, new Token(Color.YELLOW))
+}
+
+function setupReverseDiagonalWin (board: Board): void {
+  board.putToken(0, new Token(Color.RED))
+  board.putToken(0, new Token(Color.RED))
+  board.putToken(0, new Token(Color.RED))
+  board.putToken(0, new Token(Color.YELLOW))
+  board.putToken(1, new Token(Color.RED))
+  board.putToken(1, new Token(Color.RED))
+  board.putToken(1, new Token(Color.YELLOW))
+  board.putToken(2, new Token(Color.RED))
+  board.putToken(2, new Token(Color.YELLOW))
+  board.putToken(3, new Token(Color.YELLOW))
 }
